8648876a832c8c21e49a220af9bd81a5d3f91bd9
Kristoffer <kristofferk@minlejebolig.dk>
2019-02-24 19:33:26 +0100

    Add simple example program

---
+++ Snog/Snog/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Snog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string program = @"
+            Hej K-Dog,
+
+            SÆT HELTAL $a = 10
+            UDSKRIV $a
+
+            Farvel K-Dog";
+        }
+    }
+}

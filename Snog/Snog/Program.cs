33a9607db152539e94858c06eea0f9a4c7da92b6
Kristoffer <kristofferk@minlejebolig.dk>
2019-02-24 19:42:22 +0100

    Add a few token symbols

--- Snog/Snog/Program.cs
+++ Snog/Snog/Program.cs
@@ -7,12 +7,46 @@ namespace Snog
         static void Main(string[] args)
         {
             string program = @"
-            Hej K-Dog,
+            ESKETIT,
 
             SÆT HELTAL $a = 10
             UDSKRIV $a
 
-            Farvel K-Dog";
+            SKRRT SKRRT";
+
+            RunProgram(program);
+        }
+
+        private static void RunProgram(string program)
+        {
+            var lexer = SnogLexer.LoadLexer(program);
+        }
+    }
+
+    class SnogLexer
+    {
+        public string Program { get; set; }
+        private SnogLexer(string program)
+        {
+            Program = program;
+        }
+
+        public static SnogLexer LoadLexer(string program)
+        {
+            return new SnogLexer(program);
+        }
+
+        public List<Token> GetTokens()
+        {
+            return new List<Token>();
+        }
+    }
+
+    class Token
+    {
+        enum Symbols
+        {
+            START, END, ID, PRINT, INTEGER
         }
     }
 }

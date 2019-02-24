e994fde9b2c42b8c0ac22a812372481516c46dc0
Kristoffer <kristofferk@minlejebolig.dk>
2019-02-24 20:15:12 +0100

    Implement patterns for tokens

--- Snog/Snog/Program.cs
+++ Snog/Snog/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Snog
 {
@@ -11,7 +13,13 @@ namespace Snog
             ESKETIT
 
             SÆT HELTAL $a = 10
+            SÆT HELTAL $b = 15
+
+            SÆT DECIMALTAL $c = 7.5
+
             UDSKRIV $a
+            UDSKRIV $b
+            UDSKRIV $c
 
             SKRRT SKRRT";
 
@@ -21,11 +29,18 @@ namespace Snog
         private static void RunProgram(string program)
         {
             var lexer = SnogLexer.LoadLexer(program);
+            var tokens = lexer.GetTokens();
+            Console.WriteLine("Vi har nu følgende tokens:");
+            foreach (var token in tokens)
+            {
+                Console.WriteLine(token);
+            }
         }
     }
 
     class SnogLexer
     {
+        private string[] latestRegexMatch;
         public string Program { get; set; }
         private SnogLexer(string program)
         {
@@ -37,14 +52,48 @@ namespace Snog
             return new SnogLexer(program);
         }
 
-        public List<Token> GetTokens()
+        public IEnumerable<Token> GetTokens()
         {
-            var lines = Program.Split('\n');
-            for (var i = 0; i < lines.Length; i++)
-            {
+            return Program.Split('\n')
+                .Select(e => e.Trim())
+                .Where(e => !string.IsNullOrWhiteSpace(e))
+                .Select(GetTokenFromLine)
+                .ToList();
+        }
 
+        private Token GetTokenFromLine(string line)
+        {
+            if (line == "ESKETIT") return new Token(Token.Symbols.START);
+            if (line == "SKRRT SKRRT") return new Token(Token.Symbols.END);
+
+            if (LineMatches(line, "^SÆT\\s+HELTAL\\s+\\$([a-zA_Z][a-zA-Z_0-9]*)\\s*=\\s*([1-9][0-9]*)$"))
+            {
+                return new Token(Token.Symbols.INTEGER_DECLARATION, latestRegexMatch[1], latestRegexMatch[2]);
             }
-            return new List<Token>();
+
+            if (LineMatches(line, "^SÆT\\s+DECIMALTAL\\s+\\$([a-zA_Z][a-zA-Z_0-9]*)\\s*=\\s*([1-9][0-9]*(\\.[0-9]+)?)$"))
+            {
+                return new Token(Token.Symbols.FLOAT_DECLARATION, latestRegexMatch[1], latestRegexMatch[2]);
+            }
+
+            if (LineMatches(line, "^UDSKRIV\\s+\\$([a-zA_Z][a-zA-Z_0-9]*)$"))
+            {
+                return new Token(Token.Symbols.PRINT, latestRegexMatch[1]);
+            }
+
+            throw new Exception("Lexical error on: " + line);
+        }
+
+        private bool LineMatches(string line, string pattern)
+        {
+            var match = Regex.Match(line, pattern);
+            if (match.Success)
+            {
+                latestRegexMatch = match.Groups.Select(e => e.Value).ToArray();
+                return true;
+            }
+            latestRegexMatch = null;
+            return false;
         }
     }
 
@@ -52,20 +101,31 @@ namespace Snog
     {
         public enum Symbols
         {
-            START, END, ID, PRINT, INTEGER
+            START, END, ID, PRINT, INTEGER_DECLARATION, FLOAT_DECLARATION
         }
 
         public Symbols Symbol { get; set; }
         public string Value { get; set; }
+        public string Value2 { get; set; }
 
         public Token(Symbols symbol): this(symbol, null)
         {
         }
 
-        public Token(Symbols symbol, string value)
+        public Token(Symbols symbol, string value) : this(symbol, value, null)
+        {
+        }
+
+        public Token(Symbols symbol, string value, string value2)
         {
             Symbol = symbol;
             Value = value;
+            Value2 = value2;
+        }
+
+        public override string ToString()
+        {
+            return $"{Symbol} {Value} {Value2}";
         }
     }
 }
